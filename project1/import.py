1eb7ede60d5af10c4e8a44d3a09b3f296d668a59
LeeRude11 <hov3r@yandex.ru>
2018-10-01 19:59:53 +0500

    Remove finished TODOs, added 1

--- project1/import.py
+++ project1/import.py
@@ -11,6 +11,8 @@ if not os.getenv("DATABASE_URL"):
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
+# TODO enable engine to parse generators
+# engine.execution_options(stream_results=True)
 db = scoped_session(sessionmaker(bind=engine))
 
 
@@ -52,15 +54,12 @@ def write_book_to_db(entry):
 
 
 def parse_csv_w_function(csv_name, apply_func):
-    # TODO general function to get dicts based on first row headers
     with open(csv_name) as csv_file:
         entries = csv.reader(csv_file)
         headers = entries.__next__()
         # TODO pass generator to query execute many? timeit
         for entry in entries:
             entry_dict = dict(zip(headers, entry))
-            # TODO here I pass dict to DB INPUT function
-            # when this whole function exits, call db.commit()
             apply_func(entry_dict)
 
 

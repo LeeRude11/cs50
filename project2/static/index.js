a314505cc0320677d5d41771a2cf20ba783e5a81
LeeRude11 <hov3r@yandex.ru>
2018-10-27 10:41:58 +0500

    Get history on connect; addMessage()

--- project2/static/index.js
+++ project2/static/index.js
@@ -1,20 +1,39 @@
 window.onload = function() {
   var socket = io.connect('http://' + document.domain + ':' + location.port);
-  socket.on('connect', function() {
-    socket.emit('my event', {data: 'I\'m connected!'});
+
+  var messages = document.querySelector('#messages')
+  socket.on('load history', function(history) {
+    history.forEach(function(message) {
+      addMessage(message)
+    })
   });
-  var display_name = localStorage.getItem("username")
-  if (!display_name) {
-    document.querySelector('#bar-name').innerHTML = 'Guest'
-    document.querySelector('#enter').onsubmit = function(form) {
-      let form_name = document.querySelector('#display-name').value
-      socket.emit('enter', {name: form_name});
-      // TODO delete the form
-      localStorage.setItem("username", form_name)
-      return false
-    }
-  } else {
+  var display_name = localStorage.getItem('username') || 'Guest'
+  document.querySelector('#enter').onsubmit = function(form) {
+    let form_name = document.querySelector('#display-name').value
+    socket.emit('enter', {name: form_name});
     // TODO delete the form
+    localStorage.setItem('username', form_name)
+    display_name = form_name
     document.querySelector('#bar-name').innerHTML = display_name
+    return false
   }
+  document.querySelector('#bar-name').innerHTML = display_name
+
+  document.querySelector('#send').onsubmit = function(form) {
+    // TODO check if logged in
+    let text = document.querySelector('#message-text').value
+    socket.emit('send', {name: display_name, text: text});
+    return false
+  }
+
+  socket.on('receive', function(message) {
+    addMessage(message)
+  })
+
+}
+
+function addMessage(message) {
+  let new_message = document.createElement('div')
+  new_message.innerHTML = message['name'] + ": " + message['text']
+  messages.appendChild(new_message)
 }

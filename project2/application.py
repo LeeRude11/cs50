5e14ecb2f7f883090678c50c07d5f290d87321d5
LeeRude11 <hov3r@yandex.ru>
2018-11-01 14:25:50 +0500

    switch_room(), check_user(), enter_live_room()

--- project2/application.py
+++ project2/application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from collections import deque
 
 app = Flask(__name__)
@@ -25,6 +25,7 @@ def index():
 
 @socketio.on("connect")
 def connect():
+    # TODO reconnecting to a restarted app causes all kinds of trouble
     pass
 
 
@@ -33,93 +34,85 @@ def authenticate(data):
     user = data.get("username")
     if user == "Guest":
         last_room = "main"
-    elif users.get(user) is None:
-        emit("error", "No such user")
-    elif users[user]["sid"] is not None:
-        emit("error", "User is active")
+    elif check_user(user, auth=True) is False:
+        return None
     else:
         last_room = users[user]["room"]
+        users[user]["sid"] = request.sid
 
     join_room(last_room)
-    emit("load history", list(channels[last_room]["history"]))
+    enter_live_room(user, last_room)
 
 
 @socketio.on("disconnect")
 def disconnect():
-    # TODO remove SID from the user
-    request.sid
-    users
-    return None
+    # TODO sloppy search
+    user = next((k for k, v in users.items() if v["sid"] == request.sid),
+                "Guest")
+    if user != "Guest":
+        users[user]["sid"] = None
+        room = users[user]["room"]
+    else:
+        room = "main"
+    try:
+        channels[room]["current_users"].remove(user)
+    except(ValueError):
+        print("\nNon-existing user disconnected\n")
+    emit("notify", {"user": user, "action": "disconnect"}, room=room)
 
 
 @socketio.on("register")
-def login(data):
+def register(data):
     user = data.get("username")
     if user == "Guest":
-        emit("error", "Guest not allowed")
-        return None
-    if user in users:
+        emit("error", "Guest as name is not allowed")
+    elif user in users:
         emit("error", "User already exists")
-        return None
-    users[user] = {"sid": request.sid, "room": "main"}
-    emit("registered", user)
+    else:
+        room = "main"
+        users[user] = {"sid": request.sid, "room": room}
+        channels[room]["current_users"].remove("Guest")
+        channels[room]["current_users"].append(user)
+        emit("registered", user)
+        emit("notify", {"user": user, "action": "register"}, room=room)
 
 
 @socketio.on("create channel")
 def create_channel(data):
     new_channel = data.get("name")
-    if new_channel in (None, ""):
-        emit("error")
-        return 1
-    if new_channel in channels:
-        emit("error")
-        return 1
     user = data.get("user")
     if user == "Guest":
         emit("error", "Guests can not create rooms")
+    elif check_user(user) is False:
         return None
-    if users.get(user) is None:
-        emit("error", "No such user")
-        return None
-    if users[user]["sid"] != request.sid:
-        emit("error", "Wrong SID")
-        return None
+    elif new_channel in (None, ""):
+        emit("error", "Channel name can not be empty")
+    elif new_channel in channels:
+        emit("error", "Channel with this name already exists")
+    else:
+        channels[new_channel] = {
+            "history": deque([], maxlen=LIMIT),
+            "current_users": [user]
+        }
 
-    channels[new_channel] = {
-        "history": deque([], maxlen=LIMIT),
-        "current_users": []
-    }
+        switch_rooms(user, new_channel)
 
-    leave_room(users[user]["room"])
-    join_room(new_channel)
-    users[user]["room"] = new_channel
-
-    emit("channel created", new_channel, broadcast=True)
+        emit("channel created")
+        emit("new channel", new_channel, broadcast=True)
 
 
 @socketio.on("join")
 def on_join(data):
+    # TODO guests are restricted
     user = data.get("user")
     room = data.get("room")
-    if user is None or room is None:
-        emit("error", "Nones passed")
+    if room not in channels:
+        emit("error", "No such room")
+    elif check_user(user) is False:
         return None
-    if users.get(user) is None:
-        emit("error", "No such user")
-        return None
-    if users[user]["sid"] != request.sid:
-        emit("error", "Wrong SID")
-        return None
-
-    last_room = users[user]["room"]
-    send(user + ' has left the room.', room=last_room)
-    leave_room(last_room)
-    join_room(room)
-    users[user]["room"] = room
-
-    # TODO send emit?
-    emit("load history", list(channels[room]["history"]))
-    send(user + ' has entered the room.', room=room)
+    else:
+        switch_rooms(user, room)
+        enter_live_room(user, room)
 
 
 @socketio.on("send")
@@ -127,11 +120,7 @@ def send_message(data):
     user = data.get("user")
     if user == "Guest":
         room = "main"
-    elif users.get(user) is None:
-        emit("error", "No such user")
-        return None
-    elif users[user]["sid"] != request.sid:
-        emit("error", "SID is not correct")
+    elif check_user(user) is False:
         return None
     else:
         room = users[user]["room"]
@@ -142,3 +131,34 @@ def send_message(data):
     }
     channels[room]["history"].append(message)
     emit("receive", message, room=room)
+
+
+def switch_rooms(user, new_room):
+    last_room = users[user]["room"]
+    channels[last_room]["current_users"].remove(user)
+    emit("notify", {"user": user, "action": "leave"}, room=last_room)
+    leave_room(last_room)
+    join_room(new_room)
+    users[user]["room"] = new_room
+
+
+def check_user(user, auth=False):
+    if users.get(user) is None:
+        emit("error", "No such user")
+    elif auth:
+        if users[user]["sid"] is not None:
+            emit("error", "User is active")
+    elif users[user]["sid"] != request.sid:
+        emit("error", "Wrong SID")
+    else:
+        return True
+    return False
+
+
+def enter_live_room(user, room):
+    channels[room]["current_users"].append(user)
+    emit("load room", {
+        "history": list(channels[room]["history"]),
+        "users": channels[room]["current_users"]
+        })
+    emit("notify", {"user": user, "action": "enter"}, room=room)

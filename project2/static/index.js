f2d193096419201a7da5174b30c42d3349156913
LeeRude11 <hov3r@yandex.ru>
2018-11-04 11:26:02 +0500

    Callback fns for 'register' and 'create room'; 'auth' into 'connect' event

--- project2/static/index.js
+++ project2/static/index.js
@@ -2,52 +2,42 @@ window.onload = function() {
   var socket = io.connect('http://' + document.domain + ':' + location.port);
 
   var messages = document.querySelector('#messages')
+  var display_name = localStorage.getItem('username') || 'Guest'
+
+  socket.on('connect', function() {
+
+    socket.emit('authenticate', {username: display_name}, (user) => {
+      document.querySelector('#bar-name').textContent = user
+      // TODO remove from local storage?
+    })
+  });
 
   socket.on('load room', function(data) {
-    while (messages.firstChild)
-      messages.removeChild(messages.firstChild)
+    clearMessages()
     data.history.forEach(function(message) {
       addMessage(message)
     })
     console.log(data.users)
   });
-  var display_name = localStorage.getItem('username') || 'Guest'
-
-  socket.emit('authenticate', {username: display_name})
-  // TODO only if successful
-  document.querySelector('#bar-name').textContent = display_name
 
   document.querySelector('#register').onsubmit = function(form) {
     let form_name = document.querySelector('#display-name').value
-    socket.emit('register', {username: form_name});
+    socket.emit('register', {username: form_name}, (user) => {
+      if (user != undefined) {
+        localStorage.setItem('username', user)
+        display_name = user
+        document.querySelector('#bar-name').textContent = user
+      }
+    });
     return false
   }
 
-  socket.on('registered', function(user) {
-    localStorage.setItem('username', user)
-    display_name = user
-    document.querySelector('#bar-name').textContent = user
-  })
-
   document.querySelector('#send-message').onsubmit = function(form) {
-    // TODO check if logged in
     let text = document.querySelector('#message-text').value
     socket.emit('send', {user: display_name, text: text});
     return false
   }
 
-  document.querySelector('#create-room').onsubmit = function(form) {
-    let name = document.querySelector('#room-name').value
-    socket.emit('create room', {name: name, user: display_name});
-    return false
-  }
-  socket.on('room created', function(data) {
-    while (messages.firstChild)
-      messages.removeChild(messages.firstChild)
-    // TODO users list
-  })
-
-  // TODO callback in emit send?
   socket.on('receive', function(message) {
     addMessage(message)
   })
@@ -59,9 +49,20 @@ window.onload = function() {
     })
   })
 
+  document.querySelector('#create-room').onsubmit = function(form) {
+    let name = document.querySelector('#room-name').value
+    socket.emit('create room', {name: name, user: display_name}, (data) => {
+      if (data != undefined) {
+        clearMessages()
+      }
+    });
+    return false
+  }
+
   socket.on('new room', function(room_name) {
     let new_room = document.createElement('li')
     let new_room_link = document.createElement('a')
+    new_room_link.href = '#'
     new_room_link.textContent = room_name
     new_room_link.addEventListener('click', function(){
       socket.emit('join', {
@@ -82,4 +83,10 @@ function addMessage(message) {
   let new_message = document.createElement('div')
   new_message.textContent = message['user'] + ": " + message['text']
   messages.appendChild(new_message)
+}
+
+// TODO clear up user list too
+function clearMessages() {
+  while (messages.firstChild)
+    messages.removeChild(messages.firstChild)
 }
